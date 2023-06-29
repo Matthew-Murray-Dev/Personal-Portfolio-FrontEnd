570b8a21c1654aad6946116a24f70099a6277ab2
Matthew Murray <matthewc.murray94@gmail.com>
2023-06-29 18:10:48 -0500

    console log test

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@
  */
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
 
-console.log(process.env)
+
 
 /**
  * Defines the default headers for these functions to work with `json-server`
